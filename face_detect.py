be87f2734df46a70f514efff318b0e071b90aefe
MagicMirror PI <services@linusbf.com>
2019-04-09 10:21:06 +0200

    Basic face-detection scripts combined

--- face_detect.py
+++ face_detect.py
@@ -3,17 +3,14 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('/Users/melkermossberg/Desktop/py-face-detect/venv/obama-phone.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def face_in_frame(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-if len(faces) < 1: print('no face')
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    if len(faces) < 1: return False
 
-for (x,y,w,h) in faces:
-    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
-cv2.imshow('img',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imwrite('last_face.png', img)
+    return True
