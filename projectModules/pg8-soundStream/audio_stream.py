4749d562ede94688a0db7b07bd2c68ec32d34099
Federspiel <josef.federspiel@gmail.com>
2019-04-25 17:09:34 +0200

    Temp commit

--- projectModules/pg8-soundStream/audio_stream.py
+++ projectModules/pg8-soundStream/audio_stream.py
@@ -2,23 +2,38 @@ import sys
 from os import path
 import pyaudio
 import wave
+from utils import audio_int
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
 RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
+THERESHOLD =  audio_int(50)
 
 p = pyaudio.PyAudio()
 
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
+    audio2send = []
+    cur_data = ''  # current chunk  of audio data
+    rel = RATE / CHUNK
+    slid_win = deque(maxlen=SILENCE_LIMIT * rel)
+    # Prepend audio from 0.5 seconds before noise was detected
+    prev_audio = deque(maxlen=PREV_AUDIO * rel)
+    started = False
+    n = num_phrases
+    response = []
 
-print("* recording")
+    while (num_phrases == -1 or n > 0):
+        cur_data = stream.read(CHUNK)
+        slid_win.append(math.sqrt(abs(audioop.avg(cur_data, 4))))
+        #print slid_win[-1]
+        if(sum([x > THRESHOLD for x in slid_win]) > 0):
+            if(not started):
+                print "Starting record of phrase"
+                started = True
+            audio2send.append(cur_data)
+        elif (started is True):
+            print "Finished"
 
 frames = []
 
