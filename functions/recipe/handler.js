e584c0757ecc708d443da9f7c03908f6065073e7
MelkerMossberg <melker.mossberg@gmail.com>
2019-05-09 09:50:35 +0200

    Get url, Webscraping tite, img, list works.

--- functions/recipe/handler.js
+++ functions/recipe/handler.js
@@ -2,28 +2,81 @@
 
 const axios = require('axios');
 const rp = require('request-promise');
-const otcsv = require('objects-to-csv');
 const cheerio = require('cheerio');
 
-function getURL() {
-    axios.get('https://www.food2fork.com/api/search?key=8c4c0167f29a3056495f6d125a67eed4&q=pork')
-        .then(response => {
-            //console.log(response.data.recipes[0].f2f_url);
-            const url = response.data.recipes[0].f2f_url;
-            return url;
-        })
-        .catch(error => {
-            console.log(error);
+
+const scrapeRecipe = () => {
+    return new Promise(((resolve, reject) => {
+        getRecipeURL()
+            .then(scrapeURL)
+            .then(recipe => {
+                resolve({
+                    headers: {"Content-Type": "application/json"},
+                    status: 200,
+                    body: JSON.stringify({data: recipe})
+                });
+
+            })
+            .catch(error => {
+                reject({
+                    headers: {"Content-Type": "application/json"},
+                    status: 500,
+                    body: JSON.stringify({data: error})
+                });
+            });
+    }))
+};
+
+const getRecipeURL = () => {
+    return new Promise((resolve, reject) => {
+
+        axios.get('https://www.food2fork.com/api/search?key=8c4c0167f29a3056495f6d125a67eed4&q=pork')
+            .then(response => {
+                //console.log(response.data.recipes[0].f2f_url);
+                resolve(response.data.recipes[0].f2f_url);
+            })
+            .catch(error => {
+                reject(console.log(error));
+            });
+    })
+};
+
+const scrapeURL = (url) => {
+    return new Promise(((resolve, reject) => {
+        rp(url)
+            .then(html => {
+                const $ = cheerio.load(html);
+                const title = extractTitle($);
+                const img = extractImage($);
+                const ingr = extractIngredients($);
+                resolve({
+                    title: title,
+                    img: img,
+                    ingr: ingr
+                })
+            })
+            .catch(error => {
+                reject(console.log(error));
+            })
+    }))
+};
+
+const extractTitle = ($) => {
+    return $('.recipe-title').text();
+}
+
+const extractImage = ($) => {
+    return $('.recipe-image').attr('src');
+}
+
+const extractIngredients = ($) => {
+    const list = [];
+    const ul = $('.about-container > ul').find('li')
+        .each((index, element) => {
+            list.push(cheerio(element).text());
         });
+    return list;
 }
 
 
-async function getRecipe() {
-    const url = 'http://food2fork.com/view/46946' //await getURL();
-    const html = await rp(url);
-    const title = cheerio('h1.recipe-title', html).text();
-    console.log(title);
-    return;
-}
-
-getRecipe();
+scrapeRecipe().then(res => console.log(res));
