896a0dae5e56ecd0f3000b629818b1ea2aaa5154
Linus Bein Fahlander <contact@linusbf.com>
2019-05-10 16:26:08 +0200

    First implementation of handling intents

--- functions/talk-to-df/handler.js
+++ functions/talk-to-df/handler.js
@@ -3,41 +3,31 @@
 const uuid = require('uuid');
 const dialogFlow = require("dialogflow");
 const waveFile = require("wavefile");
+const axios = require("axios");
+
+let IBM_USER = "";
+let IBM_PASS = "";
+let IBM_URL = "";
 
 function talkToDF(params) {
     return new Promise((resolve, reject) => {
         const file = params.data.soundFile || null;
         const fileRate = params.data.fileRate || null;
+        IBM_USER = params.IBM_USER;
+        IBM_PASS = params.IBM_PASS;
+        IBM_URL = params.IBM_URL;
         if (file) {
             const wavFile = new waveFile();
             wavFile.fromBase64(file);
             wavFile.fromALaw();
             getIntentFromSound(wavFile.toBuffer(), fileRate, params.GA_EMAIL, params.GA_KEY)
-                .then(res => {
-                    if(res.intent) {
-                        resolve({
-                            headers: {"Content-Type": "application/json"},
-                            status: 200,
-                            body: JSON.stringify({
-                                data: {
-                                    query: res.queryText,
-                                    intent: res.intent.displayName,
-                                    confidence: res.intentDetectionConfidence,
-                                    message: res.fulfillmentText
-                                },
-                                module: "pg8-avatarConversation"
-                            }) //TODO - Change module to handle response
-                        })
-                    } else {
-                        resolve({
-                            headers: {"Content-Type": "application/json"},
-                            status: 200,
-                            body: JSON.stringify({
-                                data: {res},
-                                module: "pg8-avatarConversation"
-                            })
-                        })
-                    }
+                .then(handleIntent)
+                .then(result => {
+                    resolve({
+                        headers: {"Content-Type": "application/json"},
+                        status: 200,
+                        body: JSON.stringify(result) //TODO - Change module to handle response
+                    })
                 })
                 .catch(err => reject({
                     headers: {"Content-Type": "application/json"},
@@ -92,6 +82,54 @@ function getIntentFromSound(buffer, rate, gaEmail, gaKey) {
             })
             .catch(err => reject(err));
     });
+}
+
+function handleIntent(dfResponse) {
+    return new Promise(async (resolve, reject) => {
+        if (dfResponse.intent.displayName === "Show recipe") {
+            const recipeRes = await runRecipeFunc(["beef", "onion"]);
+            resolve({
+                data: {
+                    query: dfResponse.queryText,
+                    intent: dfResponse.intent.displayName,
+                    confidence: dfResponse.intentDetectionConfidence,
+                    message: {
+                        action: "RECIPE_SHOW",
+                        recipes: recipeRes.data,
+                    }
+                },
+                module: "pg8-recipes"
+            })
+        } else {
+            resolve({
+                data: {
+                    query: dfResponse.queryText,
+                    intent: dfResponse.intent.displayName,
+                    confidence: dfResponse.intentDetectionConfidence,
+                    message: dfResponse.fulfillmentText
+                },
+                module: "pg8-avatarConversation"
+            })
+        }
+    });
+}
+
+async function runRecipeFunc(ingredients) {
+    const res = await axios.get({
+        method: "POST",
+        url: IBM_URL,
+        auth: {
+            username: IBM_USER,
+            password: IBM_PASS
+        },
+        data: {
+            data: {
+                ingredients: ingredients
+            }
+        }
+    });
+
+    return res.data;
 }
 
 exports.talkToDF = talkToDF;
