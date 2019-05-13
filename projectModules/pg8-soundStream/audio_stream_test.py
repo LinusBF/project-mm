aa8bf1f7e265aebb743a6a40da153724c07a4676
Federspiel <josef.federspiel@gmail.com>
2019-05-13 16:00:14 +0200

    Finalized Testing for Sound Stream

--- projectModules/pg8-soundStream/audio_stream_test.py
+++ projectModules/pg8-soundStream/audio_stream_test.py
@@ -3,17 +3,18 @@ import audio_stream
 
 class TestStringMethods(unittest.TestCase):
 
-    def setUp(self):
+    def test_deletefile(self):
+        f = None
         f = open("doesExist.txt", "w+")
-
-    def test_deleteFile(self):
+        f.write("Content")
         self.assertTrue(audio_stream.delete_speech("doesExist.txt"),"File Deleted")
         self.assertFalse(audio_stream.delete_speech("doesNotExist.txt"),"File Does not exist")
+        f.close()
 
-    def test_SoundStream(self):
-        if "output_" not in audio_stream.listen_to_speech():
+    def test_soundstream(self):
+        out = None
+        if "output_" in audio_stream.listen_to_speech():
             out = True
-
         self.assertTrue(out,"File Built")
 
 if __name__ == '__main__':
